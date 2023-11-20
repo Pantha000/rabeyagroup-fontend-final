e3e854c61d76e4ffaff6fb0af6dcb725aede1c2d
new-name <email for my second account>
2023-11-20 22:37:06 +0600

    first commit

---
+++ src/redux/actions/pdfAction.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export const meterialPdf = (userData, id) => async (dispatch) => {
+  try {
+    dispatch({ type: "GetMeterialExpensesPdfRequest" });
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(
+      `/api/v1/meterial/genarate/pdf/${id}`,
+      userData,
+      config
+    );
+    dispatch({ type: "GetMeterialExpensesPdfSuccess", payload: data });
+  } catch (err) {
+    dispatch({
+      type: "GetMeterialExpensesPdfFail",
+      payload: err.response.data.message,
+    });
+  }
+};
+
+export const labourPdf = (userData, id) => async (dispatch) => {
+  try {
+    dispatch({ type: "GetLabourExpensesPdfRequest" });
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(
+      `/api/v1/labour/genarate/pdf/${id}`,
+      userData,
+      config
+    );
+    dispatch({ type: "GetLabourExpensesPdfSuccess", payload: data });
+  } catch (err) {
+    dispatch({
+      type: "GetLabourExpensesPdfFail",
+      payload: err.response.data.message,
+    });
+  }
+};
+//Clearing Errors
+export const clearError = () => async (dispatch) => {
+  dispatch({ type: "ClearErrors" });
+};
+
+//Clearing Success
+export const clearSuccess = () => async (dispatch) => {
+  dispatch({ type: "ClearSuccess" });
+};
